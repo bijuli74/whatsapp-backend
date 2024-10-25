bf720cb1e982fda2df3f28bd2396222a06f9c2c9
bijuli74 <rust_bondcr007@yahoo.com>
2024-10-26 02:49:53 +0530

    update GlobalExceptionHandler with other exception methods

--- src/main/java/com/bijuli/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bijuli/exception/GlobalExceptionHandler.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.bijuli.dto.ErrorDetail;
 
@@ -31,4 +33,23 @@ public class GlobalExceptionHandler {
     return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
   }
 
+  @ExceptionHandler(NoHandlerFoundException.class)
+  public ResponseEntity<ErrorDetail> noHandlerFoundExceptionHandler(NoHandlerFoundException e, WebRequest request) {
+    ErrorDetail err = new ErrorDetail(e.getMessage(), request.getDescription(false), LocalDateTime.now());
+    return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorDetail> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e,
+      WebRequest request) {
+    ErrorDetail err = new ErrorDetail(e.getMessage(), request.getDescription(false), LocalDateTime.now());
+    return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorDetail> otherExceptionHandler(Exception e, WebRequest request) {
+    ErrorDetail err = new ErrorDetail(e.getMessage(), request.getDescription(false), LocalDateTime.now());
+    return new ResponseEntity<>(err, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 }
