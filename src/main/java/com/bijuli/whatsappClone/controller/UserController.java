135b89a8e738761ffa772f7f8367ebb67f9b19f8
bijuli74 <rust_bondcr007@yahoo.com>
2024-11-08 11:49:15 +0530

    post to put user/update

--- src/main/java/com/bijuli/whatsappClone/controller/UserController.java
+++ src/main/java/com/bijuli/whatsappClone/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,7 +40,7 @@ public class UserController {
     return new ResponseEntity<List<User>>(users, HttpStatus.OK);
   }
 
-  @PostMapping("/update")
+  @PutMapping("/update")
   public ResponseEntity<ApiResponse> updateUserHandler(@RequestBody UpdateUserRequest request,
       @RequestHeader("Authorization") String token)
       throws UserException {
